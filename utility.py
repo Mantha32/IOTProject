e63cdbd0fb1738fa297e6bb09eec2a21f2415a2c
Doriane Kaffo <dorianekaffo@macbook-air-de-doriane.home>
2019-06-05 23:12:11 +0200

    utility

--- utility.py
+++ utility.py
@@ -4,20 +4,28 @@ import dateutil.parser
 
 # Decode an uplink message payload from a buffer
 # From base64 payload to hex to string decoder
+
+
 def decoder(payload):
     messageByte = base64.b64decode(payload)
     return messageByte.decode('utf-8')
 
 # Convert bytes in hexadecimal to string
+
+
 def hex_to_string(payload):
     byte_to_string = binascii.hexlify(bytearray(payload))
     return byte_to_string.decode('utf-8', 'strict')
 
 # Remove header in the payload
+
+
 def remove_payload_header(header, payload):
     return payload[len(header):]
 
 # Convert ISO datetime to datetime.datetime, drop millisecond value
+
+
 def iso_to_datetime(dtr_str):
     my_date = dateutil.parser.parse(dtr_str)
 
